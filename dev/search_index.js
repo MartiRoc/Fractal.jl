66ec0f630c2dbd62e12b5f7e69ebbc77fd44c0a8
Documenter.jl <documenter@juliadocs.github.io>
2023-01-18 21:50:53 +0000

    build based on 3790bbd

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Fractal","category":"page"},{"location":"#Fractal","page":"Home","title":"Fractal","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Fractal.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Fractal]","category":"page"}]
+}
